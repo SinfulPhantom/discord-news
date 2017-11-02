5141d2f1307e0fc14a1543888a573bf470455a89
Andrew Corsini <andrewcameron.corsini@gmail.com>
2017-11-02 02:02:42 -0500

    Changes

--- bot.js
+++ bot.js
@@ -28,17 +28,19 @@ bot.on('message', function(user, userID, channelID, message, event) {
   if (message.substring(0, 1) == '!') {
     var args = message.substring(1).split(' ');
     var cmd = args[0];
-
     args = args.splice(1);
     switch (cmd) {
       case 'sources':
-        var category;
-        if (args.length > 1) {
-          category = args[1];
-        }
-        var response = news.getSources(category);
-        console.log(response);
-        bot.sendMessage({to: channelID, message: response});
+        var category = args[0];
+        var language = args[1];
+        var country = args[2];
+        var sources = news.getSources(category, language, country);
+        var output = '';
+        sources.forEach(function(item, index, array)
+        {
+          output = output.concat(item + '\n');
+        });
+        bot.sendMessage({to: channelID, message: output});
         break;
 
       case 'ping':
--- news.js
+++ news.js
@@ -1,5 +1,5 @@
 var newsApi = require('./auth.json');
-const request = require('request');
+//const request = require('https');
 const fs = require('fs');
 //Class Constructor
 function News(){};
@@ -7,35 +7,24 @@ function News(){};
 //gets news sources
 //Currently retrieves only english sources
 //Currently retrieves USA sources
-News.prototype.getSources = function (category)
+News.prototype.getSources = function (category, language, country)
 {
-  var requestURL = '';
-  var result = '';
-  if(typeof category == 'undefined' || category == '')
-  {
-    requestURL = newsApi.News.sources+'?language=en&country=us' + '&apiKey=' + newsApi.News.token;
-  }
-  else
-  {
-    requestURL = newsApi.News.sources+'?category='+category+ '?language=en&country=usa' +'&apiKey='+newsApi.News.token;
-  }
-  console.log('Request URL: '+requestURL);
-  var response = request(requestURL,{json:true}, (error,response,body) =>
-  {
-    if(error)
+  var requestURL = newsApi.News.sources+'?';
+  var result = [];
+  requestURL = requestURL.concat(((category==null || category=='') ? '' : 'category='+category+'&'));
+  requestURL = requestURL.concat(((language==null || language=='') ? '' : 'language='+language+'&'));
+  requestURL = requestURL.concat(((country==null || country=='') ? '' : 'country='+country+'&'));
+  requestURL = requestURL.concat('apiKey='+newsApi.News.token);
+  var request = require('request');
+  request.get(requestURL, (err, resp, body) => {
+    let json = JSON.parse(body);
+    for(var i in json.sources)
     {
-      return "Unable to retrieve news sources at this time.. Sorry!";
-    }
-    else
-    {
-      fs.writeFile('response.txt', response.body.sources);
-      return response;
+      //console.log(json.sources[i].id);
+      result.push(json.sources[i].id);
     }
   });
-  for(var index in response)
-  {
-    console.log(response[index]);
-  }
+  console.log("Result: " + result.length);
   return result;
 };
 
